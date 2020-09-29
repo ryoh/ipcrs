b0e50f8da7ab2f922184d7252113b0b5e4b37cf1
Ryoh Kawai <kawairyoh@gmail.com>
2020-09-29 23:35:46 +0900

    Add argparser

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    ipaddr: String,
+}
+
+fn main() -> Result<()>{
+    let opt = Opt::from_args();
+
+    println!("{:?}", opt);
+
+    Ok(())
 }
