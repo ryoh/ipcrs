8112fe4f445e8f799925ea631949249f948170c7
Ryoh Kawai <kawairyoh@gmail.com>
2020-09-30 00:04:57 +0900

    parse IpAddr type

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use std::net::IpAddr;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -6,10 +7,12 @@ struct Opt {
     ipaddr: String,
 }
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let opt = Opt::from_args();
 
-    println!("{:?}", opt);
+    let ipaddr: IpAddr = opt.ipaddr.parse()?;
+
+    println!("{:?}", ipaddr);
 
     Ok(())
 }
